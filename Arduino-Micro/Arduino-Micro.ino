5507b9b3fb24ca78ce10804d782dc028a27ba172
Nize2200 <164642703+Nize2200@users.noreply.github.com>
2024-09-25 16:23:50 +0200

    added 8 buttons and feedback on button press
    Doom editiot

--- Arduino-Micro/Arduino-Micro.ino
+++ Arduino-Micro/Arduino-Micro.ino
@@ -1,59 +1,98 @@
+#include <pitches.h>
+
 #include <XInput.h>
 
-const uint8_t Pin_clicker1 = 9;
-const uint8_t Pin_clicker2 = 8;
-
+const int Buzzer = 1;
 const uint8_t Pin_buttonA = 2;
 const uint8_t Pin_buttonB = 3;
 const uint8_t Pin_buttonX = 4;
 const uint8_t Pin_buttonY = 5;
-const uint8_t Pin_buttonL3 = 7;
-
-
+const uint8_t Pin_buttonDU = 6;
+const uint8_t Pin_buttonDD = 7;
+const uint8_t Pin_buttonDR = 8;
+const uint8_t Pin_buttonDL = 9;
+const uint8_t Pin_JoyX = A1;
+const uint8_t Pin_JoyY = A0;
 
 const int DEADZONE = 1;
 
 void setup() {
-    pinMode(Pin_clicker1, INPUT_PULLUP);
-    pinMode(Pin_clicker2, INPUT_PULLUP);
+    pinMode(Buzzer,OUTPUT);
     pinMode(Pin_buttonA, INPUT_PULLUP);
     pinMode(Pin_buttonB, INPUT_PULLUP);
     pinMode(Pin_buttonX, INPUT_PULLUP);
     pinMode(Pin_buttonY, INPUT_PULLUP);
-    pinMode(Pin_buttonL3, INPUT_PULLUP);
+    pinMode(Pin_buttonDU, INPUT_PULLUP);
+    pinMode(Pin_buttonDD, INPUT_PULLUP);
+    pinMode(Pin_buttonDR, INPUT_PULLUP);
+    pinMode(Pin_buttonDL, INPUT_PULLUP);
+    XInput.setRange(JOY_LEFT, 0, 1023);
     XInput.begin();
 }
 
 void loop() {
-    boolean pressClicker1 = !digitalRead(Pin_clicker1);
-    boolean pressClicker2 = !digitalRead(Pin_clicker2);
-    boolean pressButton1 = !digitalRead(Pin_buttonA);
-    boolean pressButton2 = !digitalRead(Pin_buttonB);
-    boolean pressButton3 = !digitalRead(Pin_buttonX);
-    boolean pressButton4 = !digitalRead(Pin_buttonY);
-    boolean pressButton5 = !digitalRead(Pin_buttonL3);
+    boolean pressButtonA = !digitalRead(Pin_buttonA);
+    boolean pressButtonB = digitalRead(Pin_buttonB);
+    boolean pressButtonX = digitalRead(Pin_buttonX);
+    boolean pressButtonY = !digitalRead(Pin_buttonY);
+    boolean pressButtonDU = !digitalRead(Pin_buttonDU);
+    boolean pressButtonDD = !digitalRead(Pin_buttonDD);
+    boolean pressButtonDR = !digitalRead(Pin_buttonDR);
+    boolean pressButtonDL = !digitalRead(Pin_buttonDL);
 
-    XInput.setButton(BUTTON_LB, pressClicker1); // Map Button 1 to "LB"
-    XInput.setButton(BUTTON_RB, pressClicker2); // Map Button 2 to "RB"
-    XInput.setButton(BUTTON_A, pressButton1);   // Map Button 3 to "A"
-    XInput.setButton(BUTTON_B, pressButton2);   // Map Button 4 to "B"
-    XInput.setButton(BUTTON_X, pressButton3);   // Map Button 5 to "X"
-    XInput.setButton(BUTTON_Y, pressButton4);   // Map Button 6 to "Y"
-    XInput.setButton(BUTTON_L3, pressButton5);
+    XInput.setButton(BUTTON_A, pressButtonA);   // Map Button 3 to "A"
+   // XInput.setButton(BUTTON_B, pressButtonB);   // Map Button 4 to "B"
+    //XInput.setButton(BUTTON_X, pressButtonX);   // Map Button 5 to "X" X SKA BLI LEFT B SKA BLI RIGHT
+    XInput.setButton(BUTTON_Y, pressButtonY);   // Map Button 6 to "Y"
+    XInput.setButton(DPAD_UP,pressButtonDU);
+    XInput.setButton(DPAD_DOWN,pressButtonDD);
+    XInput.setButton(DPAD_RIGHT,pressButtonDR);
+    XInput.setButton(DPAD_LEFT,pressButtonDL);
+    int16_t leftXAxis = analogRead(Pin_JoyX);
+    int16_t leftYAxis = analogRead(Pin_JoyY);
 
-    int16_t leftXAxis = (analogRead(A0) - 512) *64;
-    int16_t leftYAxis = (analogRead(A1) - 512) *64;
+    XInput.setJoystick(JOY_RIGHT,0,0,pressButtonB,pressButtonX);
+   // XInput.setTrigger(TRIGGER_RIGHT, pressButton1);
 
     // Apply deadzone
-    if (abs(leftXAxis) < DEADZONE) leftXAxis = 1023/2;
-    if (abs(leftYAxis) < DEADZONE) leftYAxis = 1023/2;
+    //if (abs(leftXAxis) < DEADZONE) leftXAxis = 1023/2;
+    //if (abs(leftYAxis) < DEADZONE) leftYAxis = 1023/2;
   
   
-  XInput.setJoystick(JOY_RIGHT, leftXAxis,1023/2);
+  XInput.setJoystickX(JOY_LEFT, leftXAxis, false);
   XInput.setJoystickY(JOY_LEFT, leftYAxis, true);
     uint16_t rumble = XInput.getRumble();
 
+
+
+    if(pressButtonDU){
+      tone(Buzzer,NOTE_D4);
+    }
+    else {
+      noTone(Buzzer);
+    }
+    
+    if(pressButtonDD){
+      tone(Buzzer,NOTE_F4);
+    }
+    else {
+      noTone(Buzzer);
+    }
+    
+    if(pressButtonDR){
+      tone(Buzzer,NOTE_G4);
+    }
+    else {
+      noTone(Buzzer);
+    }
+    
+    if(pressButtonDL){
+      tone(Buzzer,NOTE_GS4);
+    }
+    else {
+      noTone(Buzzer);
+    }
     if (rumble > 0) {
-      
+
     }
 }
