fa822013973cb491e4e8110ab6f6c19157088b86
Nize2200 <164642703+Nize2200@users.noreply.github.com>
2024-09-27 14:58:20 +0200

    First prototype finnished
    All buttons and joysticks functinal for the first working handgrip

--- Arduino-Micro/Arduino-Micro.ino
+++ Arduino-Micro/Arduino-Micro.ino
@@ -1,23 +1,66 @@
+#include <X360ControllerLEDs.h>
+
 #include <pitches.h>
 
 #include <XInput.h>
+const uint8_t Pin_buttonLB = 0;
+const uint8_t Pin_buttonRB = 1;
+const uint8_t Pin_buttonA = 4;
+const uint8_t Pin_buttonB = 5;
+const uint8_t Pin_buttonX = 2;
+const uint8_t Pin_buttonY = 3;
+const uint8_t Pin_buttonDU = 7; //DU = D-pad Upp
+const uint8_t Pin_buttonDD = 8; //DD = D-pad Down
+const uint8_t Pin_buttonDR = 9; //DR = D-pad Rright
+const uint8_t Pin_buttonDL = 6; //DL = D-pad Left
 
-const int Buzzer = 1;
-const uint8_t Pin_buttonA = 2;
-const uint8_t Pin_buttonB = 3;
-const uint8_t Pin_buttonX = 4;
-const uint8_t Pin_buttonY = 5;
-const uint8_t Pin_buttonDU = 6;
-const uint8_t Pin_buttonDD = 7;
-const uint8_t Pin_buttonDR = 8;
-const uint8_t Pin_buttonDL = 9;
+
 const uint8_t Pin_JoyX = A1;
 const uint8_t Pin_JoyY = A0;
+const uint8_t Pin_JoyRightX = A2;
+const uint8_t Pin_JoyRightY = A3;
 
+
+const uint8_t Pin_LED_1 = 10;
+const uint8_t Pin_LED_2 = 11;
+const uint8_t Pin_LED_3 = 12;
+const uint8_t Pin_LED_4 = 13;
 const int DEADZONE = 1;
+#define BUZZER_PIN 1
+
+
+// DOOM intro
+int melody[] = {
+  NOTE_E2, NOTE_E2, NOTE_E3, NOTE_E2, NOTE_E2, NOTE_D3, NOTE_E2, NOTE_E2, 
+  NOTE_C3, NOTE_E2, NOTE_E2, NOTE_AS2, NOTE_E2, NOTE_E2, NOTE_B2, NOTE_C3,
+  NOTE_E2, NOTE_E2, NOTE_E3, NOTE_E2, NOTE_E2, NOTE_D3, NOTE_E2, NOTE_E2,
+  NOTE_C3, NOTE_E2, NOTE_E2, NOTE_AS2,
+  
+  NOTE_E2, NOTE_E2, NOTE_E3, NOTE_E2, NOTE_E2, NOTE_D3, NOTE_E2, NOTE_E2, 
+  NOTE_C3, NOTE_E2, NOTE_E2, NOTE_AS2, NOTE_E2, NOTE_E2, NOTE_B2, NOTE_C3,
+  NOTE_E2, NOTE_E2, NOTE_E3, NOTE_E2, NOTE_E2, NOTE_D3, NOTE_E2, NOTE_E2,
+  NOTE_C3, NOTE_E2, NOTE_E2, NOTE_AS2,
+  
+};
+
+int durations[] = {
+  8, 8, 8, 8, 8, 8, 8, 8, 
+  8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 2,
+  
+  8, 8, 8, 8, 8, 8, 8, 8, 
+  8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 2,
+  
+};
+
 
 void setup() {
-    pinMode(Buzzer,OUTPUT);
+  
+    pinMode(Pin_buttonRB, INPUT_PULLUP);
+    pinMode(Pin_buttonLB, INPUT_PULLUP);
     pinMode(Pin_buttonA, INPUT_PULLUP);
     pinMode(Pin_buttonB, INPUT_PULLUP);
     pinMode(Pin_buttonX, INPUT_PULLUP);
@@ -26,73 +69,116 @@ void setup() {
     pinMode(Pin_buttonDD, INPUT_PULLUP);
     pinMode(Pin_buttonDR, INPUT_PULLUP);
     pinMode(Pin_buttonDL, INPUT_PULLUP);
+    pinMode(Pin_LED_1,OUTPUT);
+    pinMode(Pin_LED_1,OUTPUT);  
+    pinMode(Pin_LED_1,OUTPUT);  
+    pinMode(Pin_LED_1,OUTPUT);
+
     XInput.setRange(JOY_LEFT, 0, 1023);
+    XInput.setRange(JOY_RIGHT, -100 ,1123);
     XInput.begin();
 }
 
 void loop() {
+
+  int size = sizeof(durations) / sizeof(int);
+
+  int playerIndex = XInput.getPlayer();
+
+  digitalWrite(Pin_LED_1, LOW);
+  digitalWrite(Pin_LED_2, LOW);
+  digitalWrite(Pin_LED_3, LOW);
+  digitalWrite(Pin_LED_4, LOW);
+
+
+ 
+  // Turn on the LED corresponding to the player index
+  switch (playerIndex) {
+    case 0:
+      digitalWrite(Pin_LED_1, HIGH);
+      break;
+    case 1:
+      digitalWrite(Pin_LED_2, HIGH);
+      break;
+    case 2:
+      digitalWrite(Pin_LED_3, HIGH);
+      break;
+    case 3:
+      digitalWrite(Pin_LED_4, HIGH);
+      break;
+  }
+    boolean pressButtonRB = !digitalRead(Pin_buttonRB);
+    boolean pressButtonLB = !digitalRead(Pin_buttonLB);
     boolean pressButtonA = !digitalRead(Pin_buttonA);
-    boolean pressButtonB = digitalRead(Pin_buttonB);
-    boolean pressButtonX = digitalRead(Pin_buttonX);
+    boolean pressButtonB = !digitalRead(Pin_buttonB);
+    boolean pressButtonX = !digitalRead(Pin_buttonX);
     boolean pressButtonY = !digitalRead(Pin_buttonY);
     boolean pressButtonDU = !digitalRead(Pin_buttonDU);
     boolean pressButtonDD = !digitalRead(Pin_buttonDD);
     boolean pressButtonDR = !digitalRead(Pin_buttonDR);
     boolean pressButtonDL = !digitalRead(Pin_buttonDL);
 
-    XInput.setButton(BUTTON_A, pressButtonA);   // Map Button 3 to "A"
-   // XInput.setButton(BUTTON_B, pressButtonB);   // Map Button 4 to "B"
-    //XInput.setButton(BUTTON_X, pressButtonX);   // Map Button 5 to "X" X SKA BLI LEFT B SKA BLI RIGHT
-    XInput.setButton(BUTTON_Y, pressButtonY);   // Map Button 6 to "Y"
+    
+    //XInput.setButton(BUTTON_LB,pressButtonLB);
+    //XInput.setButton(BUTTON_RB,pressButtonRB);
+    XInput.setButton(BUTTON_A, pressButtonA);   
+    XInput.setButton(BUTTON_B, pressButtonB);   
+    XInput.setButton(BUTTON_X, pressButtonX); 
+    XInput.setButton(BUTTON_Y, pressButtonY);   
     XInput.setButton(DPAD_UP,pressButtonDU);
     XInput.setButton(DPAD_DOWN,pressButtonDD);
     XInput.setButton(DPAD_RIGHT,pressButtonDR);
     XInput.setButton(DPAD_LEFT,pressButtonDL);
     int16_t leftXAxis = analogRead(Pin_JoyX);
     int16_t leftYAxis = analogRead(Pin_JoyY);
-
-    XInput.setJoystick(JOY_RIGHT,0,0,pressButtonB,pressButtonX);
-   // XInput.setTrigger(TRIGGER_RIGHT, pressButton1);
+    int16_t rightXAxis = analogRead(Pin_JoyRightX);
+    int16_t rightYAxis = analogRead(Pin_JoyRightY);
+   
+    
 
     // Apply deadzone
     //if (abs(leftXAxis) < DEADZONE) leftXAxis = 1023/2;
     //if (abs(leftYAxis) < DEADZONE) leftYAxis = 1023/2;
   
-  
+
+    if(pressButtonRB) {
+
+      XInput.press(TRIGGER_RIGHT);
+    }
+    else {
+
+      XInput.release(TRIGGER_RIGHT);
+    }
+    
+    if(pressButtonLB) {
+
+      XInput.press(TRIGGER_LEFT);
+    }
+    else {
+
+      XInput.release(TRIGGER_LEFT);
+    }
   XInput.setJoystickX(JOY_LEFT, leftXAxis, false);
-  XInput.setJoystickY(JOY_LEFT, leftYAxis, true);
+  XInput.setJoystickY(JOY_LEFT, leftYAxis, true); 
+  XInput.setJoystickX(JOY_RIGHT, rightYAxis, true); // Joystik is mounted wrong way on joystick , makesure we check x and y
+  XInput.setJoystickY(JOY_RIGHT, rightXAxis, true);
     uint16_t rumble = XInput.getRumble();
 
 
-
+    // if we press a button and want something to happen
     if(pressButtonDU){
-      tone(Buzzer,NOTE_D4);
-    }
-    else {
-      noTone(Buzzer);
-    }
+
+      digitalWrite(Pin_LED_4, HIGH);
+      digitalWrite(Pin_LED_3, HIGH);
+     }
     
-    if(pressButtonDD){
-      tone(Buzzer,NOTE_F4);
-    }
-    else {
-      noTone(Buzzer);
-    }
     
-    if(pressButtonDR){
-      tone(Buzzer,NOTE_G4);
-    }
-    else {
-      noTone(Buzzer);
-    }
     
-    if(pressButtonDL){
-      tone(Buzzer,NOTE_GS4);
-    }
-    else {
-      noTone(Buzzer);
-    }
     if (rumble > 0) {
+       
 
     }
+    
+
 }
+
