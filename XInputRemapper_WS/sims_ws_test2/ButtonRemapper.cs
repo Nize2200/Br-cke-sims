08f3ca220231b28a4c02d2ba6e9383390e3ed8ea
beno2101 <beno2101@student.miun.se>
2024-10-18 10:22:37 +0200

    Uppload a WS version

---
+++ XInputRemapper_WS/sims_ws_test2/ButtonRemapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.XInput;
+
+namespace XInputRemapper
+{
+    public class ButtonMapper
+    {
+        private Gamepad changedController;
+        private List<(GamepadButtonFlags from, GamepadButtonFlags to)> remaps;
+
+        public ButtonMapper()
+        {
+            changedController = new Gamepad();
+            remaps = new List<(GamepadButtonFlags from, GamepadButtonFlags to)>();
+        }
+
+        public void AddRemap(GamepadButtonFlags fromButton, GamepadButtonFlags toButton)
+        {
+            remaps.Add((fromButton, toButton));
+        }
+
+        public Gamepad MapButtons(Gamepad originalController)
+        {
+            changedController = originalController;
+
+            foreach (var remap in remaps)
+            {
+                if ((originalController.Buttons & remap.from) != 0)
+                {
+                    changedController.Buttons |= remap.to;
+                    if (remap.from != remap.to)
+                    {
+                        changedController.Buttons &= ~remap.from;
+                    }
+                }
+            }
+
+            return changedController;
+        }
+    }
+}
---
+++ XInputRemapper_WS/sims_ws_test2/DatabaseHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace XInputRemapper
+{
+    public class DatabaseHandler
+    {
+        private string connectionString = "Host=localhost;Port=5432;Username=sims;Password=123qwe;Database=seeeds";
+
+        public void AddToDatabase(int index, string stateJson, string commandJson)
+        {
+            try
+            {
+                using (var conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (var cmd = new NpgsqlCommand("INSERT INTO xinput_list (index_, state_, command_) VALUES (@index, @state::jsonb, @command::jsonb)", conn))
+                    {
+                        cmd.Parameters.AddWithValue("index", index);
+                        cmd.Parameters.AddWithValue("state", NpgsqlTypes.NpgsqlDbType.Jsonb, stateJson);
+                        cmd.Parameters.AddWithValue("command", NpgsqlTypes.NpgsqlDbType.Jsonb, commandJson);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error adding value to database: {ex.Message}");
+            }
+        }
+    }
+}
